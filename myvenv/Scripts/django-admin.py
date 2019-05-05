f58b5febf07b86d7729c80c78db2f060701df663
SunJoon-Lee <tjswns1002@gmail.com>
2019-05-05 13:46:26 +0900

    생애 첫 뉴비톤 협업결과!

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tjswn\desktop\newb!ethon_2019\newbie\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
